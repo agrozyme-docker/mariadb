870fee05433d943bebf624537cf1586fd38b06d8
agrozyme <agrozyme@gmail.com>
2019-02-13 06:16:38 +0800

    update

--- rootfs/usr/local/bin/docker-build.lua
+++ rootfs/usr/local/bin/docker-build.lua
@@ -5,7 +5,7 @@ local function main()
   core.run("apk add --no-cache mariadb mariadb-client")
   core.run("mkdir -p /var/log/mysql /usr/local/etc/mysql")
   core.link_log(nil, "/var/log/mysql/error.log")
-  core.append_file("/etc/my.cnf", "\n !includedir /etc/mysql/ \n !includedir /usr/local/etc/mysql/ \n")
+  core.append_file("/etc/my.cnf", "\n!includedir /etc/mysql/ \n!includedir /usr/local/etc/mysql/ \n")
 end
 
 main()
